2d011c2ddcb95f77987346f07dfaa36e9bc0989d
Syed Arhan Rizvi <arhanrizvi9@gmail.com>
2024-07-07 16:54:24 +0530

    Pushing my whole completed code

---
+++ src/App.jsx
@@ -0,0 +1,76 @@
+
+import './App.css'
+import Nav from './components/Nav'
+import Section from './components/Sections'
+
+function App() {
+  return (
+    <body>
+      <Nav></Nav>
+      <Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Meer Hadi"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atqueLorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Meer Taqi Meer"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Haji Ali"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt." author={"Owais"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Amir Ali"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atqueLorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Izmam Hasan"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Mohammad"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias " author={"Mukhtar"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Ghazi Ali"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atqueLorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque fugiat nulla dolorum nemo, temporibus distinctio possimus quod hic provident asperiores quas? Totam necessitatibus est blanditiis placeat, aliquid neque dolorem sunt.
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Haider Abbas"}></Section>
+
+<Section data="Lorem ipsum dolor sit amet consectetur, adipisicing elit..
+Iste quasi ratione, rem optio architecto obcaecati ipsa fugit molestias nam saepe aspernatur adipisci reprehenderit accusantium enim at inventore earum. Alias repudiandae facere ipsum, vitae excepturi recusandae assumenda dolorem atque" author={"Ali Haider"}></Section>
+<section className="w-full flex justify-center">
+        <div className="w-1/2 p-6 flex flex-wrap gap-5 justify-center">
+          <div className="flex w-3/4 justify-center">
+           <div className='-mr-5'> <img src="https://images.g2crowd.com/uploads/product/image/social_landscape/social_landscape_0b8322f146a8cd84d3fd4f953bcd5493/enterprise-architect.png" className="h-16 w-28" alt="" /></div>
+            <h1 className="font-bold text-3xl text-blue-700 mt-2 font-serif">Logoipsum</h1>
+          </div>
+          <div className="w-3/4 text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem nobis earum officiis praesentium eaque maxime quaerat animi perspiciatis natus saepe. Et voluptatem accusantium velit? Commodi, quos officia. Id, atque adipisci!
+          </div>
+          <div className="w-3/4 font-mono flex gap-6 justify-center">
+            <a href="">About</a>
+            <a href="">Career</a>
+            <a href="">History</a>
+            <a href="">Service</a>
+            <a href="">Project</a>
+            <a href="">Blog</a>
+          </div>
+          <div className="w-3/4 flex gap-3 justify-center">
+            <img className="h-7 w-9" src="https://www.pngarts.com/files/16/FB-Icon-Facebook-Logo-Grey-Circle-PNG.png" alt="" />
+            <img className="h-7 w-9" src="https://icones.pro/wp-content/uploads/2021/06/icone-github-grise-300x300.png" alt="" />
+            <img className="h-7 w-9" src="https://th.bing.com/th/id/R.7b418b95265465c6304ed0404386e011?rik=T201BtL1wsLrtg&riu=http%3a%2f%2feternal-emotion.com%2fwp-content%2fuploads%2f2018%2f05%2finstagram-icon-grey.png&ehk=4FGt5qmr1dz7yLY3ro2TbzvdvcnhMDZhQ5G8lyP3dkI%3d&risl=&pid=ImgRaw&r=0" alt="" />
+            <img className="h-7 w-9" src="https://th.bing.com/th/id/OIP.zrprNx66kA-L1iUpyZx1gwHaHa?rs=1&pid=ImgDetMain" alt="" />
+            <img className="h-7 w-9" src="https://toppng.com/public/uploads/thumbnail/logo-whatsapp-png-blanco-whatsapp-icon-grey-11562899974yzrdqqnncs.png" alt="" />
+          </div>
+        </div>
+      </section>
+      
+    </body>
+  )
+}
+
+export default App
